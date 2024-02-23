4e1457da5960978b293ac5ad175672bfdfc2ee81
Vineeth Reddy B <vineethreddyys@gmail.com>
2024-02-23 17:31:46 +0530

    practice update

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ export const App = () => {
 
 export default App*/
 
-import React ,{useState}from 'react'
+/*import React ,{useState}from 'react'
 
 export const App = () => {
   const[user,setUser] = useState("")
@@ -118,7 +118,304 @@ export const App = () => {
   )
 }
 
+export default App*/
+
+/*import React,{useState} from 'react'
+
+export const App = () => {
+  const[data,setData] = useState({
+    username:'',
+    password:'',
+
+  })
+  
+  const{username,password} = data;
+  const onChange = e=>{
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+  const onSubmitHandler = e=>{
+    e.preventDefault();
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <center>
+      <form onSubmit = {onSubmitHandler}>
+      <input type = "text" placeholder = "username" name = "username" value={username} onChange = {onChange}/>
+      <br/>
+      <input type = "password" placeholder = "Password" name = "password" value={password} onChange = {onChange}/>
+      <br/>
+      <input type = "submit"/>
+      </form>
+      </center>
+    </div>
+  )
+}
+export default App*/
+
+/*import React from 'react'
+
+export const App = () => {
+  const arr = ["React js","Node js","Express js","Angular js"]
+  return (
+    <div>
+      {
+        arr.map(
+          (value,index) => 
+          (
+            <li key = {index}>{value}</li>
+            
+          )
+        )
+      }
+    </div>
+  )
+}
+export default App*/
+/*import React from 'react';
+
+export const App = () => {
+  const courses = [
+    { id: 1, name: "React js" },
+    { id: 2, name: "Node js" },
+    { id: 3, name: "Angular js" },
+    { id: 4, name: "Express js" }
+  ];
+  return (
+    <div>
+      
+        {courses.map((value) => (
+          <li key={value.id}>{value.name}</li>
+        ))}
+      
+    </div>
+  );
+};
+
+export default App;*/
+
+/*import React from 'react'
+
+export const App = () => {
+  const names = ['James','John','Paul','Ringo','George'];
+  const filtered = names.filter((name)=> name.includes('J'))
+  return (
+    <div>
+      {
+        filtered.map((item)=>
+        (
+          <li>{item}</li>
+        )
+        )
+      }
+    </div>
+  )
+}
+export default App*/
+
+/*import React from 'react'
+
+export const App = () => {
+  const numbers = [10,12,3,40,50];
+  const filtered = numbers.filter((number)=>number>20)
+  return (
+    <div>
+      {
+        filtered.map((item)=><li>{item}</li>)
+      }
+    </div>
+  )
+}
+export default App*/
+
+
+/*import React from 'react'
+import Home from './components/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+
+export const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+
+    </div>
+  )
+}
+export default App*/
+
+
+/*import React,{useState} from 'react'
+
+export const App = () => {
+  const[data,setData] = useState({
+    username : "",
+    password : ""
+  })
+  const{username,password} = data;
+  const onChange = e => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
+  const submitHandler = e=>
+  {
+    e.preventDefault();
+    console.log(data);
+  }
+  
+  return (
+    <div>
+      <center>
+        <form onSubmit = {submitHandler}>
+          <input type = "text" name = "username" placeholder = "Username" onChange = {onChange}/>
+          <br/>
+          <input type = "password" name = "password" placeholder = "Password" onChange = {onChange}/>
+          <br/>
+          <input type = "submit"/>
+
+        </form>
+      </center>
+    </div>
+  )
+}
+export default App*/
+
+/*import React ,{useState}from 'react'
+
+export const App = () => {
+  const [data,setData] = useState(
+    {
+      username: '',
+      email: '',
+      password:'',
+      confirmPassword:'',
+    }
+
+  )
+  
+  const{username,email,password,confirmPassword} = data;
+  const onChange = e=>
+  {
+    setData({...data,[e.target.name]:e.target.value})
+  }
+  const onSubmit = e=>
+  {
+    e.preventDefault();
+    if(username.length <= 5)
+    {
+      alert("username must be at least 5 characters")
+    }
+    else if(password !== confirmPassword)
+    {
+      alert("passwords are not matching")
+    }
+    else{
+      console.log(data);
+    }
+  }
+
+  return (
+    <div>
+      <center>
+        <form onSubmit = {onSubmit}>
+          <input type = "text" name = "username" value = {username} placeholder = "Username" onChange = {onChange}/>
+          <br/>
+          <input type = "email" name = "email" value = {email}  placeholder = "email" onChange = {onChange}/>
+          <br/>
+          <input type = "password" name = "password" value = {password} placeholder = "Password" onChange = {onChange}/>
+          <br/>
+          <input type = "password" name = "confirmPassword" value = {confirmPassword} placeholder = "Confirm Password" onChange = {onChange}/>
+          <br/>
+          <input type = "submit" name = "submit"/>
+
+        </form>
+      </center>
+    </div>
+  )
+}
+export default App*/
+
+
+
+/*import React,{useState} from 'react'
+
+export const App = () => {
+  const[input,setInput] = useState("");
+  const[result,setResult] = useState(0);
+  const handler = e=>
+  {
+    setInput(e.target.value);
+  }
+
+
+  return (
+
+    <div>
+      <br/>
+      <center>
+        <input type = "text" onChange = {handler} name = "input" value = {input}/>
+        <br/>
+        <br/>
+        
+        
+        <button onClick = {()=> setResult(eval(input))}>Result </button>
+        <h4>Result is : {result}</h4>
+      </center>
+    </div>
+  )
+}
+export default App;*/
+
+/*import React,{useState,useEffect} from 'react'
+
+
+export const App = () => {
+
+  const[count,setCount] = useState(0);
+  useEffect(()=>console.log(count),[count])
+  
+  return (
+    <div>
+      <button onClick = {()=>setCount(count+1)}>Click</button>
+      <h1>{count}</h1>
+    </div>
+  )
+}
+export default App*/
+
+import React,{useState,useEffect} from 'react'
+
+export const App = () => {
+  const[data,setData] = useState([]);
+
+  useEffect(()=>
+  {
+    console.log("useefffect")
+    fetch("https://gorest.co.in/public/v2/users/").then(
+      RESPONSE => RESPONSE.json()
+    ).then(json => setData(json))
+  },[])
+
+  
+  
+  return (
+    <div>
+      {data.map(item => <li key = {item.id}>{item.name}</li>)}
+    </div>
+  )
+}
 export default App
+
+
+
+
+
+
+
 
 
 
